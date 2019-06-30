e7159652844af7a8f848808136a06973e3428b26
Marie-Carrere <marie.carrere@icloud.com>
2019-06-30 17:49:09 +0200

    :tada: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import GuessCount from './GuessCount'
+import Card from './Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return (
+    <div className="memory">
+      <GuessCount guesses={0} />
+      <Card card="😄" feedback="hidden" />
+      <Card card="💖" feedback="justMatched" />
+      <Card card="🎩" feedback="justMismatched" />
+      <Card card="🐶" feedback="visible" />
+      <Card card="🐱" feedback="hidden" />
+      <Card card="🎉" feedback="justMatched" />
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './Card.css'
+
+const HIDDEN_SYMBOL = '❓'
+
+const Card = ({ card, feedback }) => (
+    <div className={`card ${feedback}`}>
+        <span className="symbol">
+            {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
+        </span>
+
+    </div>
+)
+
+export default Card
