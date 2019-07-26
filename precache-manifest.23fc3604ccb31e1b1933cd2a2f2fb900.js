e6074946ac36507d8c9735b4d7b62f6fb4720218
Marie-Carrere <marie.carrere@icloud.com>
2019-07-26 11:19:43 +0200

    Updates

---
+++ precache-manifest.23fc3604ccb31e1b1933cd2a2f2fb900.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc067d9d9f6da4775167a7cdea3471eb",
+    "url": "/react-memory/index.html"
+  },
+  {
+    "revision": "1ecc9dc572a77990bf8f",
+    "url": "/react-memory/static/css/main.4f9ec027.chunk.css"
+  },
+  {
+    "revision": "f307f8388ba32d6d5574",
+    "url": "/react-memory/static/js/2.75372809.chunk.js"
+  },
+  {
+    "revision": "1ecc9dc572a77990bf8f",
+    "url": "/react-memory/static/js/main.e5033994.chunk.js"
+  },
+  {
+    "revision": "591630390940aacf310a",
+    "url": "/react-memory/static/js/runtime~main.fbbade95.js"
+  }
+]);
